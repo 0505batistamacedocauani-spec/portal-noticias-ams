ce3fd9b4a39d1bd1c490344eae01cc2dd3bd98cf
cauani de macedo <0505batistamacedocauani@gmail.com>
2026-08-26 12:33:35 -0300

    integrando javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Portal de notícias carregado com sucesso!");
+
+// Pergunta o nome do usuário e exibe mensagem personalizada
+let nomeVisitante = prompt("Qual é o seu nome?");
+alert("Olá, " + nomeVisitante + "! Bem-vindo ao Portal ETEC News.");
+console.log("Visitante ativo: " + nomeVisitante);
+
+// Pergunta se deseja ativar as notificações do site
+let desejaNoticias = confirm("Deseja receber notificações sobre novas matérias?");
+console.log("Aceitou receber notificações: " + desejaNoticias);
+<prompt>
+    
+</prompt>
